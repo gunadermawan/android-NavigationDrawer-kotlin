91e62b7f50227be46cbc0288a74478fe417a0993
Guna Dermawan <53375007+gunadermawan@users.noreply.github.com>
2021-09-28 16:29:05 +0700

    menambah menu di sidebar

--- app/src/main/java/com/gunder/mynavigationdrawer/MainActivity.kt
+++ app/src/main/java/com/gunder/mynavigationdrawer/MainActivity.kt
@@ -46,7 +46,7 @@ class MainActivity : AppCompatActivity() {
             .into(profileCircleImageView)
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow, R.id.nav_cart
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
